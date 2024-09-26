9dcb72a9cb6032d8becf2593f8a7b89d53ca7743
haruBlank <harublank00@gmail.com>
2024-09-26 23:53:43 +0545

    add daisy ui

--- apps/frontend/src/scripts/main.ts
+++ apps/frontend/src/scripts/main.ts
@@ -1,5 +1,5 @@
 import Alpine from 'alpinejs';
-
+import 'preline';
 import '../styles/styles.css';
 
 declare global {
--- apps/frontend/tailwind.config.js
+++ apps/frontend/tailwind.config.js
@@ -7,5 +7,9 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    // require('preline/plugin'),
+    require('daisyui'),
+    require("@tailwindcss/typography")
+  ],
 }
